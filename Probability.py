7da3712bb749c3fb927619c859181a5383f32e58
Mayra Vázquez-Sánchez <mayravazquez-sanchez@Mayras-Air.home>
2019-05-22 16:06:19 -0400

    Reads file and obtains frequencies of each character, still working on calculating the probability

--- Probability.py
+++ Probability.py
@@ -3,8 +3,21 @@
 def readFile():
     words = open("Words.txt", "r")
     contents = words.readlines()
-    letters = {}
+    print(contents)
+    return contents
+
+def getFrequencies(contents):
+    letters = dict()
     letter_count = 0
+    sum_of_freq_letters = 0
     for line in contents:
         for letter in line:
-            letter_count = 
+            letters[letter] = letters.get(letter, 0) + 1
+            letter_count++ 
+        sum_of_freq_letters += letter_count
+    return letters, sum_of_freq_letters
+
+def getprobabilty(letters, sum_of_freq_letters):
+    probabilities_letters = []
+    probability_letter = 
+
