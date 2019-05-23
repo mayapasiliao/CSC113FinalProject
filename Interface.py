ebe7611caadc689b11b47ad74416ca4afea5a85f
mayapasiliao <mayapasiliao@gmail.com>
2019-05-23 12:53:44 -0400

    Working button

--- Interface.py
+++ Interface.py
@@ -1,15 +1,19 @@
 from tkinter import *
+from Probability import probability
 
-def callback():
-    n.get()
-    print(n)
+# Sample function
+def show_entry_fields():
+   print(e1.get())
 
 master = Tk()
 master.title("Input")
-Label(master, text="Enter n, number of most common characters: ").grid(row=1)
-e1 = Entry(master)
-e1.grid(row=1, column=1)
-button = Button(master, text="Enter", command = probability(n.get())
-button.grid(row=2, column=0, pady=4, textvariable = n)
 
-mainloop()
+Label(master, text="Enter n, number of most common characters: ").grid(row=0)
+
+e1 = Entry(master)
+e1.grid(row=0, column=1)
+
+Button(master, text="Quit", command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
+Button(master, text="Enter", command=probability).grid(row=3, column=1, sticky=W, pady=4)
+
+mainloop( )
