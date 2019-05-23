b88b53456f65e31ccddc791ca5c3d43b4f836a99
Mayra Vázquez-Sánchez <mayravazquez-sanchez@Mayras-Air.home>
2019-05-23 14:44:05 -0400

    update

--- Probability.py
+++ Probability.py
@@ -5,7 +5,6 @@ def readFile():
     '''Reads file and returns its contents'''
     words = open("Words.txt", "r")
     contents = words.readlines()
-    print(contents)
     return contents
 
 def getFrequencies(contents):
@@ -18,8 +17,6 @@ def getFrequencies(contents):
             letters[letter] = letters.get(letter, 0) + 1
             letter_count += 1
         sum_of_freq_letters += letter_count
-    print(letters)
-    print(sum_of_freq_letters)
     return letters, sum_of_freq_letters
 
 def getProbability(letters, sum_of_freq_letters):
@@ -27,8 +24,6 @@ def getProbability(letters, sum_of_freq_letters):
     sum_probabilities = 0
     for letter, letter_count in letters.items():
         prob_of_letter = letter_count / sum_of_freq_letters
-        print(letter)
-        print(prob_of_letter)
         sum_probabilities += prob_of_letter
     return sum_probabilities
 
@@ -39,4 +34,3 @@ def probability():
     sum_probabilities = getProbability(letters, sum_of_freq_letters)
     return sum_probabilities, letters
 
-probability()
