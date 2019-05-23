1f8d62a5a02512af7ec6b52af1a448e817f3c347
mayapasiliao <mayapasiliao@gmail.com>
2019-05-23 14:30:09 -0400

    Works with sample_dictionary

--- test.py
+++ test.py
@@ -3,12 +3,13 @@ import math
 # from Probability import probability
 
 def origin(turtle):
-    turtle.penup()
-    turtle.setpos(-0,150)
+	turtle.penup()
+	turtle.setpos(-0,150)
 	my_turtle.pendown()
 
 def piesections(letter_probability):
-	return central_angle = (2*math.pi)*letter_probability
+	central_angle = (2*math.pi)*letter_probability
+	return central_angle
 
 Name_of_segment = ["h","e","l","l","o"," ","g","u","y","s","All other letters"]
 segment = [0.10,0.10,0.20,0.60]
@@ -28,12 +29,12 @@ w.title ("Pie Chart")
 r=150
 my_turtle.circle(r)
 origin(my_turtle)
-my_turtle.forward(50)
+my_turtle.forward(150)
 
 # i should be n, input from user
 for i in sample_dict:
 	angle = piesections(sample_dict["a"])
 	origin(my_turtle)
 	my_turtle.right(angle)
-	my_turtle.forward(50)
+	my_turtle.forward(150)
 	origin(my_turtle)
